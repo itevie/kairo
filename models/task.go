3ccdbace25b4a13aee6af1a8fa75d61ce1244a22
Isabella <izzyplays07@gmail.com>
2024-11-26 13:57:41 +0000

    d

--- models/task.go
+++ models/task.go
@@ -34,7 +34,7 @@ func FetchTask(id int, user int, db *sqlx.DB) (*Task, error, int) {
 }
 
 func GetTasks(user int, db *sqlx.DB, scan []Task) error {
-	if err := db.Select(&scan, "SELECT * FROM tasks WHERE user = ?;", user); err != nil {
+	if err := db.Select(scan, "SELECT * FROM tasks WHERE user = ?;", user); err != nil {
 		return errors.New("failed to load tasks from database")
 	}
 
